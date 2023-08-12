7b3aa00afe15409f8e71828d510591fde0a783ea
Juan Sanoja <juan.sanoja@gmail.com>
2023-08-12 12:46:27 -0300

    CG-001 - init commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Making public dir"
+mkdir public
+echo "Enter folder project"
+cd client
+echo "Install dependencies"
+npm i
+echo "Run build project"
+npm run build
+echo "Moving client to public dir"
+mv ./dist/client/* ../public/
+echo "Finish deploy /public/ on clasic-games!"
